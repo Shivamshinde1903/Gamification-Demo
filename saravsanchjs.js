2f61618e5e63b25f0a9b894a2c6042ed01c06246
Shivamshinde1903 <shivamkshinde1903@gmail.com>
2025-04-30 23:23:05 +0530

    modifiedsaravsanchjs.js

--- saravsanchjs.js
+++ saravsanchjs.js
@@ -2,18 +2,22 @@
 //import myJson1 from './question.json' assert {type: 'json'};
 //chanhged
 // Dynamic import
-// const myJson1 = await import('./question.json', {
-//   assert: { type: 'json' }
-// });
+const myJson1 = await import('./question.json', {
+  assert: { type: 'json' }
+});
 
 // or for CommonJS
-import fs from 'fs';
-const myJson1 = JSON.parse(fs.readFileSync('./question.json', 'utf-8'));
+// import fs from 'fs';
+// const myJson1 = JSON.parse(fs.readFileSync('./question.json', 'utf-8'));
 //chanhged
 
 //import myJson2 from './answers.json' assert {type: 'json'};
 
-const myJson2 = JSON.parse(fs.readFileSync('./answers.json', 'utf-8'));
+//const myJson2 = JSON.parse(fs.readFileSync('./answers.json', 'utf-8'));
+
+const myJson2 = await import('./answers.json', {
+  assert: { type: 'json' }
+});
 
 var gun = 0;
 let headersList = {
